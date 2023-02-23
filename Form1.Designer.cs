33845542653ed644169353fb29d075f51d299030
s8_jurcs_k <s8_jurcs_k@venta.lv>
2023-02-23 18:16:34 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Micromium
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Adress = new System.Windows.Forms.ToolStrip();
+            this.Go = new System.Windows.Forms.ToolStripButton();
+            this.MyAdress = new System.Windows.Forms.ToolStripTextBox();
+            this.BrowserTabs = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl2 = new System.Windows.Forms.TabControl();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.Adress.SuspendLayout();
+            this.BrowserTabs.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabControl2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Adress
+            // 
+            this.Adress.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Adress.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Go,
+            this.MyAdress,
+            this.toolStripButton2,
+            this.toolStripButton1});
+            this.Adress.Location = new System.Drawing.Point(0, 0);
+            this.Adress.Name = "Adress";
+            this.Adress.Size = new System.Drawing.Size(800, 25);
+            this.Adress.TabIndex = 2;
+            this.Adress.Text = "toolStrip1";
+            // 
+            // Go
+            // 
+            this.Go.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.Go.Image = ((System.Drawing.Image)(resources.GetObject("Go.Image")));
+            this.Go.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Go.Name = "Go";
+            this.Go.Size = new System.Drawing.Size(23, 22);
+            this.Go.Text = "Go";
+            this.Go.Click += new System.EventHandler(this.Go_Click);
+            // 
+            // MyAdress
+            // 
+            this.MyAdress.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.MyAdress.Name = "MyAdress";
+            this.MyAdress.Size = new System.Drawing.Size(200, 25);
+            // 
+            // BrowserTabs
+            // 
+            this.BrowserTabs.Controls.Add(this.tabPage1);
+            this.BrowserTabs.Controls.Add(this.tabPage2);
+            this.BrowserTabs.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BrowserTabs.Location = new System.Drawing.Point(0, 25);
+            this.BrowserTabs.Name = "BrowserTabs";
+            this.BrowserTabs.SelectedIndex = 0;
+            this.BrowserTabs.Size = new System.Drawing.Size(800, 425);
+            this.BrowserTabs.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.tabControl2);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(792, 399);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(192, 74);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabControl2
+            // 
+            this.tabControl2.Controls.Add(this.tabPage3);
+            this.tabControl2.Controls.Add(this.tabPage4);
+            this.tabControl2.Location = new System.Drawing.Point(777, 52);
+            this.tabControl2.Name = "tabControl2";
+            this.tabControl2.SelectedIndex = 0;
+            this.tabControl2.Size = new System.Drawing.Size(18, 13);
+            this.tabControl2.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(10, 0);
+            this.tabPage3.TabIndex = 0;
+            this.tabPage3.Text = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage4.Size = new System.Drawing.Size(10, 0);
+            this.tabPage4.TabIndex = 1;
+            this.tabPage4.Text = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton1.Text = "+";
+            this.toolStripButton1.ToolTipText = "Add tab";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton2.Image")));
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButton2.Text = "-";
+            this.toolStripButton2.ToolTipText = "Remove Tab";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BrowserTabs);
+            this.Controls.Add(this.Adress);
+            this.Name = "Form1";
+            this.Text = "Micromium";
+            this.Adress.ResumeLayout(false);
+            this.Adress.PerformLayout();
+            this.BrowserTabs.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabControl2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStrip Adress;
+        private System.Windows.Forms.ToolStripButton Go;
+        private System.Windows.Forms.ToolStripTextBox MyAdress;
+        private System.Windows.Forms.TabControl BrowserTabs;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabControl tabControl2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,101 @@
+﻿using CefSharp.WinForms;
+using CefSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Micromium
+{
+    public partial class Form1 : Form
+    {
+        ChromiumWebBrowser chromiumWebBrowser = null;
+        List<ChromiumWebBrowser> chromiumWebBrowserList = new List<ChromiumWebBrowser>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            IntializeBrowser();
+        }
+
+        private void chromiumWebBrowser1_LoadingStateChanged(object sender, CefSharp.LoadingStateChangedEventArgs e)
+        {
+
+        }
+
+        public void IntializeBrowser()
+        {
+            var settings = new CefSettings();
+            Cef.Initialize(settings);
+
+            chromiumWebBrowser = new ChromiumWebBrowser("https://google.com");
+            BrowserTabs.TabPages[0].Controls.Add(chromiumWebBrowser);
+            chromiumWebBrowser.Dock = DockStyle.Fill;
+
+            chromiumWebBrowser = new ChromiumWebBrowser("https://google.com");
+            BrowserTabs.TabPages[1].Controls.Add(chromiumWebBrowser);
+            chromiumWebBrowser.Dock = DockStyle.Fill;
+
+        }
+
+        private void Go_Click(object sender, EventArgs e)
+        {
+            //browser.Load(MyAdress.Text);
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addBrowserTab()
+        {
+            TabPage tabPage = new TabPage();
+            tabPage.Text = "Broswer Page";
+
+            chromiumWebBrowser = new ChromiumWebBrowser("https://google.com");
+            tabPage.Controls.Add(chromiumWebBrowser);
+            chromiumWebBrowser.Dock = DockStyle.Fill;
+
+            BrowserTabs.TabPages.Add(tabPage);
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            TabPage tabPage = new TabPage();
+            tabPage.Text = "Broswer Page";
+
+            chromiumWebBrowser = new ChromiumWebBrowser("https://google.com");
+            tabPage.Controls.Add(chromiumWebBrowser);
+            chromiumWebBrowser.Dock = DockStyle.Fill;
+
+            BrowserTabs.TabPages.Add(tabPage);
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(BrowserTabs.TabCount > 1)
+                {
+                    BrowserTabs.TabPages.Remove(BrowserTabs.SelectedTab);
+                }
+                else
+                {
+                    BrowserTabs.TabPages.Remove(BrowserTabs.SelectedTab);
+                    addBrowserTab();                   
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+        }
+    }
+}
